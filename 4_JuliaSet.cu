d7c7bda92efcb62f99ad039378ad56a76116b797
Deep Lalwani <deepthesmartest@gmail.com>
2023-01-28 23:59:53 +0530

    Animated Julia Set

--- 4_JuliaSet.cu
+++ 4_JuliaSet.cu
@@ -1,5 +1,5 @@
 #include"common/book.h"
-#include"common/cpu_bitmap.h"
+#include"common/cpu_anim.h"
 
 #define DIM 1000
 
@@ -23,30 +23,30 @@ struct cuComplex {
 	}
 };
 
-__device__ int julia(int x, int y) {
+__device__ int julia(int x, int y, float time) {
 	const float scale = 1.5f;
 	float jx = scale * (float)(DIM / 2 - x) / (DIM / 2);
 	float jy = scale * (float)(DIM / 2 - y) / (DIM / 2);
 
-	cuComplex c(-0.8, 0.156);
+	cuComplex c(time, time);
 	cuComplex a(jx, jy);
 
 	int i = 0;
 	for(i = 0; i < 200; i++) {
 		a = a * a + c;
 		if(a.magnitude2() > 1000) {
-			return 0;
+			return 1;
 		}
 	}
-	return 1;
+	return 0;
 }
 
-__global__ void kernel(unsigned char *ptr) {
+__global__ void kernel(unsigned char *ptr, float time) {
 	int x = blockIdx.x;
 	int y = blockIdx.y;
 	int offset = x + y * gridDim.x;
 	
-	int isJulia = julia(x, y);
+	int isJulia = julia(x, y, time);
 
 	ptr[offset*4 + 0] = 255 * isJulia;
 	ptr[offset*4 + 1] = 0;
@@ -54,16 +54,33 @@ __global__ void kernel(unsigned char *ptr) {
 	ptr[offset*4 + 3] = 255;
 }
 
-int main(void) {
-	CPUBitmap bitmap(DIM, DIM);
+struct DataBlock {
 	unsigned char *dev_bitmap;
+	CPUAnimBitmap *bitmap;
+};
 
-	HANDLE_ERROR(cudaMalloc((void**) &dev_bitmap, bitmap.image_size()));
+void cleanup(DataBlock *d) {
+	cudaFree(d->dev_bitmap);
+}
 
+void generateFrame(DataBlock *d, int ticks) {
+	static float time = 0.0f;
+	static int sign = 1;
 	dim3 grid(DIM, DIM);
-	kernel<<<grid, 1>>>(dev_bitmap);
+	kernel<<<grid, 1>>>(d->dev_bitmap, time);
+	time += 0.001f * sign;
+	if(time >= 0.4f || time <= 0.0f) {
+		sign *= -1;
+	}
 	
-	HANDLE_ERROR(cudaMemcpy(bitmap.get_ptr(), dev_bitmap, bitmap.image_size(), cudaMemcpyDeviceToHost));
-	bitmap.display_and_exit();
-	cudaFree(dev_bitmap);
+	HANDLE_ERROR(cudaMemcpy(d->bitmap->get_ptr(), d->dev_bitmap, d->bitmap->image_size(), cudaMemcpyDeviceToHost));
+}
+
+int main(void) {
+	DataBlock data;
+	CPUAnimBitmap bitmap(DIM, DIM, &data);
+	data.bitmap = &bitmap;
+
+	HANDLE_ERROR(cudaMalloc((void**) &data.dev_bitmap, bitmap.image_size()));
+	bitmap.anim_and_exit((void (*)(void*, int))generateFrame, (void (*) (void*))cleanup);
 }
